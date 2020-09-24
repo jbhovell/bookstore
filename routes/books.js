03716d479873990f13d89833c29faac6dce7b942
jianfangbhbbc <jianfang.bladen-hovell@bbc.co.uk>
2020-09-24 15:58:22 +0100

    add logic for adding books

--- routes/books.js
+++ routes/books.js
@@ -6,7 +6,7 @@ const data = JSON.parse(fs.readFileSync('books.json'));
 /* GET all books listing. */
 router.get('/', function (req, res, next) {
   if (req.query.title) {
-    const item = data.books.find(b => b.title === req.query.title);
+    const item = findBook(req.query.title);
     res.send(item);
   }
   else {
@@ -20,7 +20,7 @@ router.post('/sell', function (req, res, next) {
   const title = req.body.title;
   const price = req.body.price;
   const quantity = req.body.quantity;
-  const item = data.books.find(b => b.title === req.query.title);
+  const item = findBook(title);
   if (item && item.quantity >= quantity) {
     item.price = price;
     res.send(`sold ${req.body.quantity} ${req.body.title}`);
@@ -33,8 +33,19 @@ router.post('/sell', function (req, res, next) {
 
 /* if the book exists, update stock, otherwise, add a new entry */
 router.post('/add', function (req, res, next) {
-  console.log(`added ${req.body.quantity} ${req.body.title} to the stock`);
-  res.send(`added ${req.body.quantity} ${req.body.title} to the stock`);
+  const title = req.body.title;
+  const quantity = req.body.quantity;
+  const item = findBook(title);
+  if (item) {
+    item.quantity += quantity;
+  }
+  else {
+    const price = req.body.quantity;
+    const newItem = { title: title, lowercase_title: title.toLowerCase(), price: price, quantity: quantity }
+    data.books.push(newItem);
+  }
+  console.log(`added ${quantity} ${title} to the stock`);
+  res.send(`added ${quantity} ${title} to the stock`);
 });
 
 
@@ -42,14 +53,16 @@ router.post('/add', function (req, res, next) {
 router.post('/update', function (req, res, next) {
   const title = req.body.title;
   const price = req.body.price;
-  const item = data.books.find(b => b.title === req.query.title);
+  const item = findBook(title);
   if (item) {
     item.price = price;
-    res.send(book);
+    res.send(item);
   }
   else {
     res.send(`${title} was not found. No price update was made for the request.`)
   }
 });
+
+const findBook = title => data.books.find(b => b.lowercase_title === title.toLowerCase());
 
 module.exports = router;
--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'My Express Bookstore (EST 2020)' });
+  res.render('index', { title: 'JBH Express Bookstore (Est. 2020)' });
 });
 
 module.exports = router;
