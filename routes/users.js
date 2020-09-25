6abc57c94ba64cc77eb09e9a0b5bfeceb5614d99
jianfangbhbbc <jianfang.bladen-hovell@bbc.co.uk>
2020-09-25 21:57:50 +0100

    added functions to add, delete, update users in database

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,9 @@
 var express = require('express');
 var db = require("./database.js")
+var md5 = require("md5")
 
 var router = express.Router();
 
-/* GET home page. */
 router.get('/', function (req, res, next) {
     var sql = "select * from user"
     var params = []
@@ -18,5 +18,122 @@ router.get('/', function (req, res, next) {
         })
     });
 });
+
+router.get("/:id", (req, res, next) => {
+    var sql = "select * from user where id = ?"
+    var params = [req.params.id]
+    db.get(sql, params, (err, row) => {
+        if (err) {
+            res.status(400).json({ "error": err.message });
+            return;
+        }
+        res.json({
+            "message": "success",
+            "data": row
+        })
+    });
+});
+
+router.post("/add/", (req, res, next) => {
+    var errors = []
+    if (!req.body.password) {
+        errors.push("No password specified");
+    }
+    if (!req.body.email) {
+        errors.push("No email specified");
+    }
+    if (errors.length) {
+        app.post("/api/user/", (req, res, next) => {
+            var errors = []
+            if (!req.body.password) {
+                errors.push("No password specified");
+            }
+            if (!req.body.email) {
+                errors.push("No email specified");
+            }
+            if (errors.length) {
+                res.status(400).json({ "error": errors.join(",") });
+                return;
+            }
+            var data = {
+                name: req.body.name,
+                email: req.body.email,
+                password: md5(req.body.password)
+            }
+            var sql = 'INSERT INTO user (name, email, password) VALUES (?,?,?)'
+            var params = [data.name, data.email, data.password]
+            db.run(sql, params, function (err, result) {
+                if (err) {
+                    res.status(400).json({ "error": err.message })
+                    return;
+                }
+                res.json({
+                    "message": "success",
+                    "data": data,
+                    "id": this.lastID
+                })
+            });
+        })
+        res.status(400).json({ "error": errors.join(",") });
+        return;
+    }
+    var data = {
+        name: req.body.name,
+        email: req.body.email,
+        password: md5(req.body.password)
+    }
+    var sql = 'INSERT INTO user (name, email, password) VALUES (?,?,?)'
+    var params = [data.name, data.email, data.password]
+    db.run(sql, params, function (err, result) {
+        if (err) {
+            res.status(400).json({ "error": err.message })
+            return;
+        }
+        res.json({
+            "message": "success",
+            "data": data,
+            "id": this.lastID
+        })
+    });
+})
+
+router.patch("/update/:id", (req, res, next) => {
+    var data = {
+        name: req.body.name,
+        email: req.body.email,
+        password: req.body.password ? md5(req.body.password) : null
+    }
+    db.run(
+        `UPDATE user set 
+           name = COALESCE(?,name), 
+           email = COALESCE(?,email), 
+           password = COALESCE(?,password) 
+           WHERE id = ?`,
+        [data.name, data.email, data.password, req.params.id],
+        function (err, result) {
+            if (err) {
+                res.status(400).json({ "error": res.message })
+                return;
+            }
+            res.json({
+                message: "success",
+                data: data,
+                changes: this.changes
+            })
+        });
+})
+
+router.delete("/delete/:id", (req, res, next) => {
+    db.run(
+        'DELETE FROM user WHERE id = ?',
+        req.params.id,
+        function (err, result) {
+            if (err) {
+                res.status(400).json({ "error": res.message })
+                return;
+            }
+            res.json({ "message": "deleted", changes: this.changes })
+        });
+})
 
 module.exports = router;
