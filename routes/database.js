c60ae2472ffb21a70a3ca46f6b5738988bc4a040
jianfangbhbbc <jianfang.bladen-hovell@bbc.co.uk>
2020-10-13 09:14:01 +0100

    created store_owner and customer users in sqlite

--- routes/database.js
+++ routes/database.js
@@ -23,8 +23,8 @@ let db = new sqlite3.Database(DBSOURCE, (err) => {
                 } else {
                     // Table just created, creating some rows
                     var insert = 'INSERT INTO user (name, email, password) VALUES (?,?,?)'
-                    db.run(insert, ["admin", "admin@example.com", md5("admin123456")])
-                    db.run(insert, ["user", "user@example.com", md5("user123456")])
+                    db.run(insert, ["store_owner", "store_owner@jbh_express_store.com", md5("owner_pass")])
+                    db.run(insert, ["customer", "customer@jbh_express_store.com", md5("user_pass")])
                 }
             });
     }
